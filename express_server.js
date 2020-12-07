09e7f0c0e92e71066a04e80c08ab12d96cd5c4c5
Kelechi Obikaonu <kelechipauline@gmail.com>
2020-12-07 21:26:07 +0000

    Added ajs as the new engin

--- express_server.js
+++ express_server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+//setting ejs as the new engine
+app.set("view engine", "ejs")
+
+
+//................
 const urlDatabase = {
   'b2xVn2': "http://www.lighthouselabs.ca",
   '9sm5xK': "http://www.google.com"
@@ -10,7 +15,12 @@ const urlDatabase = {
 app.get('/', (req, res) => {
   res.send('Hello!')
 });
-
+app.get('/hello', (req, res) => {
+  res.send ("<html><body>Hello <b>World</b></body></html>\n")
+})
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase )
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 });
