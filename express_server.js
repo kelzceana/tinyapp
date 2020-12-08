2f774e7ea163b3c0e5ddf5eef9542a00750a104e
Kelechi Obikaonu <kelechipauline@gmail.com>
2020-12-08 00:31:23 +0000

    added routs for the shortURL

--- express_server.js
+++ express_server.js
@@ -5,22 +5,27 @@ const PORT = 8080;
 //setting ejs as the new engine
 app.set("view engine", "ejs")
 
-
-//................
 const urlDatabase = {
   'b2xVn2': "http://www.lighthouselabs.ca",
   '9sm5xK': "http://www.google.com"
 };
-
+//.....app routes..........
 app.get('/', (req, res) => {
   res.send('Hello!')
 });
-app.get('/hello', (req, res) => {
-  res.send ("<html><body>Hello <b>World</b></body></html>\n")
-})
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase )
 })
+app.get('/urls', (req, res) => {
+  const templatedVars = { urls: urlDatabase}
+  res.render('urls_index', templatedVars)
+})
+app.get('/urls/:shortURL', (req, res) => {
+  const templatedVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
+  res.render('urls_show', templatedVars)
+})
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 });
